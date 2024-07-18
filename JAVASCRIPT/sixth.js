2482fb9ac4c87225ee4c3226c61bff8a0b02cb84
Shiwangi Maurya <shiwangi.2125it1190@kiet.edu>
2024-07-18 15:41:20 +0530

    Update sixth.js
    1

--- JAVASCRIPT/sixth.js
+++ JAVASCRIPT/sixth.js
@@ -1,1 +1,20 @@
-console.log("Hello!!");
+// console.log("Hello!!");
+// console.log(window);
+// window.console.log("Hello!!");
+// window.alert("Hello");
+
+
+//Window object--->
+/*The window object represents an open window in a browser.It is browser's object(not JavaScript's)
+& is automatically created by browser.
+It is a global object with lots of properties & methods. */
+
+//DOM 
+/*When a web page is loaded, the browser creates a Document Object Model(DOM) of the page
+console.log--->prints elements
+console.dir--->prints object properties and methods*/
+
+console.dir(document.head);
+console.log(document.body.childNodes[1]);
+//html ko js k andr access krne ka tarika hai dom
+
